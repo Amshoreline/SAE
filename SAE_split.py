fix a bug in the calculation of

SAE_split.py
@@ -203,11 +203,13 @@ def balance_split(
     E = torch.tensor(E).to(device)
     E = E / torch.sum(E)
     K = len(bins) - 1
+    bins_arr = bins
     bins = torch.tensor(bins).to(device)
     bins[-1] += eps
     C = (bins[1 :] + bins[: -1]) / 2
     # sigmas = torch.tensor([sigma] * K).to(device)  # constant value
     sigmas = (bins[1 :] - bins[: -1]) * sigma
+    S_arr = S
     S = torch.tensor(S).to(device)
     # build cooper
     cmp_ = MaximumBalance(
@@ -236,16 +238,17 @@ def balance_split(
             bins_info = ''
             R = cmp_.R.detach().cpu().numpy()
             R_hist = []
-            hard_R = np.max((1 - W).reshape(1, -1) * S.cpu().numpy(), axis=1)
+            hard_R = np.max((1 - W).reshape(1, -1) * S_arr, axis=1)
             hard_R_hist = []
             part_res = np.argpartition(W, num_train_mols)
-            real_R = np.max(S.cpu().numpy()[inds[part_res[num_train_mols :]]][:, inds[part_res[: num_train_mols]]], axis=1)
+            real_R = np.max(S_arr[inds[part_res[num_train_mols :]]][:, inds[part_res[: num_train_mols]]], axis=1)
             real_R_hist = []
-            for bin_left, bin_right in zip(bins.cpu().tolist(), bins[1 :].cpu().tolist() + [np.inf]):
+            for bin_left, bin_right in zip(bins_arr, bins_arr[1 :] + [np.inf]):
                 bins_info += f' [{bin_left:.2f}, {bin_right:.2f}]'
                 R_hist.append(round(np.sum(W * (R >= bin_left) * (R < bin_right)), 1))
                 hard_R_hist.append(round(np.sum(W * (hard_R >= bin_left) * (hard_R < bin_right)), 1))
                 real_R_hist.append(round(np.sum((real_R >= bin_left) * (real_R < bin_right)), 1))
+            real_R_hist[-2] += real_R_hist[-1]
             real_R_hist = real_R_hist[: -1]
             #
             score = -np.mean(np.abs(np.array(real_R_hist) / num_test_mols - E.cpu().numpy()))

analyse_split.py
@@ -32,9 +32,7 @@ def main(data_dir, train_files, test_files, save_path):
     assert S.shape[1] == len(train_smis)
     max_sim_list = np.max(S, axis=1)  # shape = (#test_smis, )
     # Plot
-    num_bins = 3
-    bins = [round(item / num_bins, 2) for item in np.arange(num_bins + 1).astype(np.float32)]
-    print(bins)
+    bins = [0, 1 / 3, 2 / 3, 1.01]
     bin_counts = []
     for bin_left, bin_right in zip(bins[: -1], bins[1 :]):
         bin_counts.append(np.sum((max_sim_list >= bin_left) * (max_sim_list < bin_right)))
@@ -44,7 +42,7 @@ def main(data_dir, train_files, test_files, save_path):
     plt.title(f'Train: {S.shape[0]} Test: {S.shape[1]} Bins: {bin_counts}')
     plt.hist(
         max_sim_list,
-        bins=np.linspace(0, 1, num_bins + 1),
+        bins=bins,
         # cumulative=True, density=True, histtype='step'
     )
     plt.savefig(save_path)